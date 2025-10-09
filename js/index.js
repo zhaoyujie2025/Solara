9300d367238aa613fd217cf68f02606e2a7b650c
akudamatata <583380790@qq.com>
2025-10-09 17:13:52 +0800

    Close mobile search after playing result

--- js/index.js
+++ js/index.js
@@ -2293,6 +2293,9 @@ async function playSearchResult(index) {
         // 立即隐藏搜索结果，显示播放界面
         hideSearchResults();
         dom.searchInput.value = "";
+        if (isMobileView) {
+            closeMobileSearch();
+        }
 
         // 检查歌曲是否已在播放列表中
         const existingIndex = state.playlistSongs.findIndex(s => s.id === song.id && s.source === song.source);

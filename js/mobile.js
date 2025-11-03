5f2288a04d8fcf861911a02a8686540e19ca017a
zhaoyujie2025 <zhaozong2024@gmail.com>
2025-11-03 11:47:37 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "zzmusic";
     }
 
     function updateMobileOverlayScrim() {
